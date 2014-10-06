c68232bbb8743e532e0428e4cca1bc3f2c20938c
Ivan Herman <ivan@ivan-herman.net>
2014-10-06 13:22:58 +0200

    Displaying the result was buggy: if there was an error/warning, nothing was displayed (which is incorrect, there may be no template file, but that is only a warning).
    B.t.w.: there should be a difference in the error replies on whether it is a warning or an error

--- experiments/simple-templates-jquery/display.js
+++ experiments/simple-templates-jquery/display.js
@@ -15,16 +15,17 @@ $(document).ready( function() {
     var request = { url: dataset.url, format: $.CSV_format.JSON, filters: { capitalize: capitalize } };
     $.getCSV(request)
       .done( function(csv_data) {
+        if( csv_data.data !== undefined && csv_data.data !== null && csv_data !== "" ) {
+          target.append("<pre>" + massage(csv_data.data) + "</pre>");           
+        }
         if( csv_data.errors.length !== 0 ) {
-          target.append("<p>Error in processing the data:</p><ul>");
+          target.append("<p>Error/warning in processing the data:</p><ul>");
           csv_data.errors.forEach(function(e) {
             target.append("<li>" + e + "</li>");
           });
           target.append("</ul>");
-        } else {
-          target.append("<pre>" + massage(csv_data.data) + "</pre>"); 
-          $("div#meta").append("<pre>" + JSON.stringify(csv_data.meta,null,2) + "</pre>")          
         }
+        $("div#meta").append("<pre>" + JSON.stringify(csv_data.meta,null,2) + "</pre>")          
       })
       .fail( function(status, error) {
         target.append("<p>CSV access failed: '" + status + ", " + error + "'</p>")
@@ -34,18 +35,18 @@ $(document).ready( function() {
     var dataset = $(this).prop('dataset');
     var url     = dataset.url;
     var target  = $(this);
-    var request = { url: dataset.url, format: "turtle", filters: { capitalize: capitalize }};
+    var request = { url: dataset.url, format: $.CSV_format.TURTLE, filters: { capitalize: capitalize }};
     $.getCSV(request)
       .done( function(csv_data) {
-        // console.log(csv_data);
+        if( csv_data.data !== undefined && csv_data.data !== null && csv_data !== "" ) {
+          target.append("<pre>" + massage(csv_data.data) + "</pre>");           
+        }
         if( csv_data.errors.length !== 0 ) {
-          target.append("<p>Error in processing the data:</p><ul>");
+          target.append("<p>Error/warning in processing the data:</p><ul>");
           csv_data.errors.forEach(function(e) {
             target.append("<li>" + e + "</li>");
           });
           target.append("</ul>");
-        } else {
-          target.append("<pre>" + massage(csv_data.data) + "</pre>"); 
         }
       })
       .fail( function(status, error) {
