7f57a9e6f80adc9b10e0fe04af5232e9ccf2812b
Ivan Herman <ivan@ivan-herman.net>
2015-03-20 15:40:45 +0100

    Updated the hide.js files and add the hide.css files
    As a result, printing of the file after load results in a print/PDF containing all table annotations

--- csv2rdf/hide.js
+++ csv2rdf/hide.js
@@ -1,3 +1,34 @@
+/**
+* Script to show/hide a <div> element in a document, as a result of a 
+* corresponding <button> element's action.
+* 
+* The <div> (or other) element that is to be hidden/shown has to have an @id
+* and be of class "to-toggle". The button element must have an @data-toggle-target
+* attribute with the corresponding @id value of the <div> element, and be of class "toggle". 
+*
+* The HTML source must link this javascript, as well as the hide.css file. (The
+* CSS file ensures that the <div> element is hidden by default except when it is
+* printed, when it should be visible.)
+* 
+* Finally, the end of the HTML file should include:
+*
+*    <script type="text/javascript">
+*      (function(){
+*        hide_init();
+*      })();
+*    </script>
+*
+*
+* I work with a separate internal flags instead of toggling a class and let CSS work its way, before I hope
+* to find a way to be able to print with some sort of an initial state and not with the current status of the DOM.
+* That would require that the event handlers for 'beforeprint' and 'afterprint' should work, but, at present, they do not:-(
+*
+* * Author: Ivan Herman
+* * Licence: © Copyright W3C® SOFTWARE NOTICE AND LICENSE <http://www.w3.org/Consortium/Legal/2002/copyright-software-20021231>, Ivan Herman, 2015
+* 
+* @author Ivan Herman
+* @license W3C
+*/
 
 /**
 * For each button/div pair the current visibility is stored
@@ -8,8 +39,8 @@ var visibility_flags = {};
 * These are the strings to be displayed in the buttons, ie, 
 * set as the text content of the <button> elements
 */
-var show_text = "Show detailed annotations";
-var hide_text = "Hide detailed annotations";
+var show_text = "Show detailed table annotations";
+var hide_text = "Hide detailed table annotations";
 
 /**
 * This function is invoked when the button is pushed
@@ -20,20 +51,29 @@ var hide = function(event) {
   /* Get the button that is at the origin of the event */
   var button = event.currentTarget;
   /* get the target, ie, the id of the div element */
-  var target = button.getAttribute("data-anno-target");
+  var target = button.getAttribute("data-toggle-target");
   var tohide = document.getElementById(target);
   /* get the current visibility flag */
   var flag   = visibility_flags[target];
 
   /* hide the target */
-  tohide.style.display = flag ? "none" : "inherit";
+  tohide.style.display = flag ? "none" : "block";
 
   /* Change the button's content */
   button.innerHTML = flag ? show_text : hide_text;
 
-  /* Reset the visibility flag*/
+  /* Reset the visibility flag */
   visibility_flags[target] = !flag;
 }
+
+
+// var before_print = function(event) {
+//   alert("printing!")
+// }
+
+// var after_print = function(event) {
+//   alert("printed!")
+// }
 
 /**
 * This method is invoked *at the end* of the file; 
@@ -53,17 +93,17 @@ var hide = function(event) {
 */
 var hide_init = function() {
   var buttons = document.getElementsByTagName("button");
-  var match = window.matchMedia("screen");
   for( var i = 0; i < buttons.length; i++ ) {
-    var id = buttons[i].getAttribute("data-anno-target");
-    if( match.matches ) {
-      var tohide = document.getElementById(id);
+    if( buttons[i].className === "toggle" ) {
+      var id = buttons[i].getAttribute("data-toggle-target");
       visibility_flags[id] = false;
       buttons[i].innerHTML = show_text;
-      buttons[i].addEventListener("click", hide);
-      tohide.style.display = "none";
-    } else {
-      buttons[i].style.display = "none";      
+      buttons[i].addEventListener("click", hide);      
     }
   }
+
+  /* Take care of the before and after print events */
+  /** These do not work yet, unfortunately:-( */
+  // document.addEventListener("beforeprint",before_print);
+  // document.addEventListener("afterprint",after_print)
 }
