21121cbf8a4a9e78245a2b8030114d825bd84381
Ivan Herman <ivan@ivan-herman.net>
2015-04-09 14:09:16 +0200

    Added documentation to the replace-ed-uris script, so that other people can also use it...

--- replace-ed-uris.js
+++ replace-ed-uris.js
@@ -1,3 +1,31 @@
+/*
+*
+* Replace github.io references to /TR references.
+* The issue is as follows: when several specs are developed in parallel, it is a good idea
+* to use, for mutual references, the github.io URI-s. That ensures that the editors' drafts are always
+* correct in terms of mutual references.
+*
+* However, when publishing the documents, all those references must be exchanged against the final, /TR
+* URI-s. That process, when done manually, is boring and error prone. This script solves the issue:
+*
+* * Create a separate file with the 'conversions' array. See, e.g., https://github.com/w3c/csvw/blob/gh-pages/local-biblio.js
+*   for an example.
+* * Include a reference to that file and this to the respec code, after the inclusion of respec. E.g.:
+* ```
+*  <script class="remove" src="../local-biblio.js"></script>
+*  <script class="remove" src="https://www.w3.org/Tools/respec/respec-w3c-common"></script>
+*  <script class="remove" src="../replace-ed-uris.js"></script>
+* ```
+*
+* This function will be automatically executed when the respec source is saved in an (X)HTML file.
+* Note that
+*
+* * Links in the header part will *not* be changed. That part is usually generated automatically, and the reference to the
+*   editor's draft must stay unchanged
+* * The text content of an <a> element will also be converted (if needed). This means that the reference list may also
+*   use include the github.io address (as it should...)
+*
+*/
 respecEvents.sub("save", function() {
   $("a").not( $("div.head a") ).each( function(index) {
     var href = $(this).attr("href");
