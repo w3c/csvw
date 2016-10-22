eeb450fb957f5874958a47b157572eb710d96fa1
Dan Brickley <danbri@google.com>
2016-10-23 03:19:08 +0900

    Kinda works, with 1000 disclaimers.

--- experiments/browserdemo/hellocsv.js
+++ experiments/browserdemo/hellocsv.js
@@ -4,7 +4,6 @@ var colsByTitle = {};
 var virtualCols = {};
 var foundCols = {};
 var trim;
-
 // TODO: stuff these into an object structure
 
 var verbose = true;
@@ -32,15 +31,6 @@ verbose = false;
    colmap = meta;
    if (verbose)
        console.log("Converting row using meta: ", meta);
-   //result = convertTriple(meta);
-
-// In wrong place but worry later:
-
-
-
-
-
-///*
    var jsonld_row = [];
    var result = "";
    result = "{"
@@ -48,7 +38,6 @@ verbose = false;
    var len = Object.keys(row).length;
    for (key in row) {
      i = i+1;
-     // result += " [" + i + "/"+ len + "] ";
      result += key + ":- " + row[key] ;
      if (i < len) { result += ","; }
      result += "\n"
@@ -58,16 +47,7 @@ verbose = false;
          console.log("Doing row, processing col number ", i, " - fetching col template.");
    }
    result += "}\n" //*/
-
    return result;
- }
-
- // TODO: not needed as such, except to note the JSON-LD structure. No need N3.
- function convertTriple( t ) {
-   var o = {};
-   o["@id"] = t.subject;
-   o[t.predicate] = N3.Util.isLiteral(t.object) ? N3.Util.getLiteralValue(t.object) : t.object;
-   return JSON.stringify( o );
  }
 
  // "Expand a URI Template with row index numbers."
@@ -86,9 +66,11 @@ verbose = false;
 function expandNS(s, prefixMap) {
 //    "Expand namespace prefixes eg. 'schema:foo' becomes 'http://schema.org/foo', based on a map."
     //return re.sub( "(\w+):(\w+)(\s*)", lambda match: "{0}{1}{2}".format( prefixMap.get(match.group(1), match.group(1)+":" ) , match.group(2), match.group(3) ), s).encode("utf-8")
+    s = s.replace("schema:", "http://schema.org/");
+    s = s.replace("rdf:", "http://www.w3.org/1999/02/22-rdf-syntax-ns#");
+    s = s.replace("rdfs:", "http://www.w3.org/2000/01/rdf-schema#");
     return s; // TODO
 }
-
 
 function expandBaseHref(s, cfg) {
     // "Expand a potentially relative URI. A hack."
@@ -96,31 +78,26 @@ function expandBaseHref(s, cfg) {
     return s; // TODO
 }
 
-
 function goGoGo(rows, mapping) {
-  if (verbose)
-     console.log("Go go go!", rows, mapping);
-
   var foundCols = d3.keys(rows[0]);
-  if (verbose)
+  if (verbose) {
+     console.log("Go go go!", rows, mapping);
       console.log("foundCols: ", foundCols);
+  }
+
   // First let's examine our mapping.
-
   colDefs = mapping['tableSchema']['columns'] // pick at some useful pieces
-  if (verbose)
-     console.log("colDefs: ", colDefs);
   trim = mapping['dialect']['trim'] // fallible
-
-  if (verbose)
+  if (verbose) {
+     console.log("colDefs: ", colDefs);
       console.log("trim mode?: ", trim);
-
+  }
   for (coldef_ix in colDefs){
       col = colDefs[coldef_ix];
       workingTitle = col['titles'];
       if (!workingTitle) { workingTitle = col['name']; }
      if (verbose)
             console.log( "Column name: ",  col['name'],  " title:  ", workingTitle );
-
       colsByTitle[workingTitle] = col
       isVirtual = col['virtual'];
       if (!isVirtual in window) {
@@ -138,14 +115,11 @@ function goGoGo(rows, mapping) {
   for (row_ix in rows) {
 
     row = rows[row_ix];
-    if (verbose)
+    if (verbose) {
         console.log("Row: ", row_ix, row );
-
-    // Now we go through each column in this row:
-
-    if (verbose)
         console.log("Attempting to loop thru cols of row ", row_ix, " col count: ", Object.keys(row).length);
-    for (col_ix = 0; col_ix < Object.keys(row).length; col_ix += 1 )  {
+    }
+    for (col_ix = 0; col_ix < Object.keys(row).length; col_ix += 1 )  { // through the concrete cols
       colTitle = foundCols[col_ix];
       cellSpec = colsByTitle[colTitle]; // find template for this col
       var cellval = row[colTitle];
@@ -155,58 +129,62 @@ function goGoGo(rows, mapping) {
           console.log("cellSpec for col ", col_ix, " named ", colTitle, " is: ", cellSpec);
           console.log("looked in row, ", row);
           console.log("Initial value is: ", cellval);
-          console.log("#######");
+          console.log("############");
       }
-      // CONCRETE CELL VALUES FIRST
-      myrow_aboutUrl = expandTemplate(cellSpec['aboutUrl'], row_ix);
+      myrow_aboutUrl = expandTemplate(cellSpec['aboutUrl'], row_ix);       // CONCRETE CELL VALUES FIRST
       myrow_propertyUrl = expandTemplate(cellSpec['propertyUrl'], row_ix);
       myrow_valueUrl = expandTemplate(cellSpec['valueUrl'] || cellval, row_ix);
       dt = cellSpec['datatype'] || "rdfs:Literal";
       if (verbose)
           console.log(" ***** rough triple is ", myrow_aboutUrl, myrow_propertyUrl, myrow_valueUrl, dt);
       if (dt == "anyURI") {
-          myFactoid = " {\"@id\": \"" + expandBaseHref(myrow_aboutUrl) + "\",\"" + expandNS(myrow_propertyUrl) + "\": \""+ expandBaseHref(myrow_valueUrl +"\"}" );
+          console.log("#anyURI");
+          myFactoid = " {\"@id\": \"" + expandBaseHref(myrow_aboutUrl) + "\",\"" + expandNS(myrow_propertyUrl) + "\": \""+ expandBaseHref(myrow_valueUrl +"\"}\n" );
       } else {
-        // TODO: tweak this for string-valued values.
-        myFactoid = " {\"@id\": \"" + expandBaseHref(myrow_aboutUrl) + "\",\"" +
-            expandNS(myrow_propertyUrl) + "\": {  \"@value\":  \""+ myrow_valueUrl +"\", \"@datatype\": \"" + dt + "\" }"   ;
+            console.log("#misc.", dt);
+            if (true) {
+              myFactoid = " {\"@id\": \"" + expandBaseHref(myrow_aboutUrl) + "\",\"" + expandNS(myrow_propertyUrl) + "\": \""+ myrow_valueUrl +"\" }\n"   ;
+            } else {
+              // sort out datatyping later.
+              myFactoid = " {\"@id\": \"" + expandBaseHref(myrow_aboutUrl) + "\",\"" +
+                  expandNS(myrow_propertyUrl) + "\": {  \"@value\":  \""+ myrow_valueUrl +"\", \"@datatype\": \"" + dt["base"] + "\"} }\n"   ;
+            }
       }
 
+      json_factoids.push(myFactoid);
       if (verbose)
           console.log("JSON-LD Factoid:", myFactoid);
-      json_factoids.push(myFactoid);
+   } // real col loop
 
-    // {"@id":"https://datatank.stad.gent/4/api/dcat","http://www.w3.org/1999/02/22-rdf-syntax-ns#type":"http://www.w3.org/ns/dcat#Catalog"},
-
-    //          print "<%s> <%s> \"%s\" . # TextTriple" % (expandBaseHref(myrow_aboutUrl), expandNS(myrow_propertyUrl), myrow_valueUrl.encode("utf-8") )
-
-      // THEN VIRTUAL CELLS
-
-     /*
-     for k,v in virtualCols.items():
-         if verbose:
-             print "Virtual column: k=%s v=%s " % (k,v)
-         virtualCellSpec = virtualCols.get(k, False)
-         if virtualCellSpec:
-             vrow_aboutUrl = expandTemplate(virtualCellSpec.get('aboutUrl', "N/A"), row_i)
-             vrow_propertyUrl = expandTemplate(virtualCellSpec.get('propertyUrl', "N/A"), row_i)
-             vrow_valueUrl = expandTemplate(virtualCellSpec.get('valueUrl', "NA"), row_i)
-             print "<%s> <%s> <%s> . # VirtualTriple" % (expandNS(expandBaseHref(vrow_aboutUrl)),expandNS(vrow_propertyUrl),expandNS(expandBaseHref(vrow_valueUrl)))
-         else:
-             print "# ERROR: Failed to get VirtualCellSpec. %s " % virtualCellSpec
-     */
-
-    }
-
+   // console.log("Virtual columns!" ,virtualCols);       // THEN VIRTUAL CELLS
+   for (vcol_ix in virtualCols) {
+//     console.log("Virtual column id: ",vcol_ix, " vcolSpec: ", virtualCols[vcol_ix]);
+     virtualCellSpec = virtualCols[vcol_ix] || false;
+     // console.log("Virtual cell spec: ", virtualCellSpec);
+     if (virtualCellSpec) {
+         vrow_aboutUrl = expandTemplate(virtualCellSpec['aboutUrl'], row_ix);
+         vrow_propertyUrl = expandTemplate(virtualCellSpec['propertyUrl'], row_ix);
+         vrow_valueUrl = expandTemplate(virtualCellSpec['valueUrl'] || "NA", row_ix);
+         if (vrow_propertyUrl != "rdf:type") {
+             myVirtualFactoid = " {\"@id\": \"" + expandBaseHref(vrow_aboutUrl) + "\",\"" + expandNS(vrow_propertyUrl) + "\": { \"@id\": \""+ expandNS(vrow_valueUrl +"\"} }\n" );
+         } else {
+             myVirtualFactoid = " {\"@id\": \"" + expandBaseHref(vrow_aboutUrl) + "\", \"" + "@type" + "\":  \""+ expandNS(vrow_valueUrl) +"\" }\n";
+         }
+         json_factoids.push(myVirtualFactoid);
+        // console.log("Virtual triple: ", myVirtualFactoid)
+     }
+   } // vcol loop
   } // end row loop
 
+  // EMIT all JSON-LD
   output = '<script type="application/ld+json">[ ';
   output += json_factoids.join(', ');
   output = output.concat( "]</"); // otherwise it looks like this script finishes
   output = output.concat("script>");
+
   appendHtml(document.body, output);
   console.log(output);
-  
+
   // ... and inject it into our main HTML page.
 
 }
@@ -227,14 +205,10 @@ function importData(metafile, datafile) {
    });
    }
 
-d3.select("body").append("div").attr("id", "preview").style("margin", "5px");
-d3.select("body").append("div").attr("id", "jsondata").style("margin", "5px");
-loadCSV()
-
+  d3.select("body").append("div").attr("id", "preview").style("margin", "5px");
+  d3.select("body").append("div").attr("id", "jsondata").style("margin", "5px");
+  loadCSV()
 }
-
-
-
 
 /*
 https://github.com/mbostock/d3/wiki/CSV
